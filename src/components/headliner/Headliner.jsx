9e1bd241c44a7f6e7b422b4dae6dbbeab007bed1
Gitjunk1 <mr.juse52@gmail.com>
2021-10-29 22:43:12 +0300

    first commit

---
+++ src/components/headliner/Headliner.jsx
@@ -0,0 +1,19 @@
+import './Headliner';
+
+function Headliner() {
+  return (
+    <article className="headliner">
+      <img className="" src="" alt="" />
+      <div className="">
+        <h1 className="">Hate speech vs free speech</h1>
+        <div className="">
+          .
+        </div>
+      </div>
+      
+    Headliner
+    </article>
+  );
+}
+
+export default Headliner;
